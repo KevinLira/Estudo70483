f5f6476d4dabc54e42a6382d97af428c67d691c8
Kevin Lira <kevin.lira.gilbert@gmail.com>
2018-03-31 23:16:00 -0300

    Subida Inicial projeto.

---
+++ EstudoCertificacaoExame70483/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EstudoCertificacaoExame70483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are developing an application. The application includes a method" + 
+                             " a method named ReadFile");
+            Console.WriteLine("The ReadFile Method must meet the following requirements: ");
+            Console.WriteLine(" * it must not make changes to the data file");
+            Console.WriteLine(" * it must allow other processes to access the data file");
+            Console.WriteLine(" * it must not throw an exception if the application attempts to open"+ 
+                              " a data file that does no exist");
+
+
+        }
+
+
+        public void ReadFile(string fileName){
+
+
+        }
+    }
+}

284c0a2c6735e17727cc2f2ad1d7a9a98013d370
Kevin Lira <kevin.lira.gilbert@gmail.com>
2018-04-03 00:48:49 -0300

    Adicionando estudo de caso para leak de memoria entre String.Concat e StringBuilder.

--- EstudoCertificacaoExame70483/Program.cs
+++ EstudoCertificacaoExame70483/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
 
 namespace EstudoCertificacaoExame70483
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("You are developing an application. The application includes a method" + 
                              " a method named ReadFile");
@@ -13,14 +14,16 @@ namespace EstudoCertificacaoExame70483
             Console.WriteLine(" * it must allow other processes to access the data file");
             Console.WriteLine(" * it must not throw an exception if the application attempts to open"+ 
                               " a data file that does no exist");
-
-
+            string fn = "/Users/kevinlira/Projects/EstudoCertificacaoExame70483/EstudoCertificacaoExame70483/Program.cs";
+            ReadFile(fn);
         }
 
 
-        public void ReadFile(string fileName){
+        public static void ReadFile(string FileName){
 
-
+            var fs = File.Open(FileName, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+//            var fs1 = File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            fs.Dispose();
         }
     }
 }
